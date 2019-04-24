c44c6a8a75ea21aeca155f3bb43c4c8b4373abd0
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-04-23 20:53:37 -0400

    Initial products service created

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { StoreRoutingModule } from './store-routing.module';
 import { StoreComponent } from './store.component';
+import { ProductsService } from './products.service';
 
 @NgModule({
   declarations: [StoreComponent],
-  imports: [CommonModule, StoreRoutingModule]
+  imports: [CommonModule, StoreRoutingModule],
+  providers: [ProductsService]
 })
 export class StoreModule {}
